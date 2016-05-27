dd5aac5784a61c55c1104940f7b9c193ed18adc6
J. Heasly <jheasly@earthlink.net>
2016-05-26 19:12:49 -0700

    Improved splitter templatetag.

--- dox/templatetags/splitter.py
+++ dox/templatetags/splitter.py
@@ -6,4 +6,18 @@ register = template.Library()
 @register.filter(name='splitter')
 @stringfilter
 def splitter(value, arg):
-    return value.split(arg)
+    '''
+    Improved splitter tag in that it doesn't strip out the string being split 
+    on and doesn't add that string to the last item (where it doesn't belong!) 
+    as a list comprehension would do.
+    '''
+    outlist = []
+    split_list = value.split(arg)
+    graf_count = len(split_list)
+    for index, item in enumerate(split_list):
+        if (index + 1) < graf_count:
+            item = item + u'</p>'
+            outlist.append(item)
+        else:
+            outlist.append(item)
+    return outlist
