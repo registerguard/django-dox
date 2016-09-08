41d79a33de8a451af9a55bad16ca97819f24384e
J. Heasly <jheasly@earthlink.net>
2016-09-08 13:04:39 -0700

    Fixed RegEx error_message.

--- dox/forms.py
+++ dox/forms.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from dox import models
 
 class PageForm(forms.ModelForm):
-    
+
     content = forms.CharField(
         widget=forms.Textarea(attrs={
             'class': 'vLargeTextField',
@@ -12,10 +12,11 @@ class PageForm(forms.ModelForm):
         }),
         required=False,
     )
-    
+
     # https://code.djangoproject.com/browser/django/trunk/django/contrib/flatpages/forms.py
-    
-    url = forms.RegexField(label=_('URL'), max_length=255, regex=r'^[-\w/\.~]+$', help_text = _(u'Example: /templates/ap/hosted2/ (leading/trailing slashes required)'), error_message = _(u'This value must contain only letters, numbers, dots, underscores, dashes, slashes or tildes.'),)
-    
+
+    url = forms.RegexField(label=_('URL'), max_length=255, regex=r'^[-\w/\.~]+$', help_text = _(u'Example: /templates/ap/hosted2/ (leading/trailing slashes required)'), error_messages={'invalid': _(u'This value must contain only letters, numbers, dots, underscores, dashes, slashes or tildes.')},)
+
     class Meta:
         model = models.Page
+        fields = '__all__'
