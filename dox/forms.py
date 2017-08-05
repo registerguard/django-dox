c6d8e4a75d25d40b0229b5af01a1e38f32a9847d
J. Heasly <jheasly@earthlink.net>
2017-08-04 17:23:16 -0700

    Added Django 1.6 >= compatability.

--- dox/forms.py
+++ dox/forms.py
@@ -15,7 +15,51 @@ class PageForm(forms.ModelForm):
     
     # https://code.djangoproject.com/browser/django/trunk/django/contrib/flatpages/forms.py
     
-    url = forms.RegexField(label=_('URL'), max_length=255, regex=r'^[-\w/\.~]+$', help_text = _(u'Example: /templates/ap/hosted2/ (leading/trailing slashes required)'), error_message = _(u'This value must contain only letters, numbers, dots, underscores, dashes, slashes or tildes.'),)
+    try:
+        url = forms.RegexField(
+            label=_('URL'),
+            max_length=255,
+            regex=r'^[-\w/\.~]+$',
+            help_text = _(u'Example: /templates/ap/hosted2/ (leading/trailing slashes required)'),
+            error_message = _(u'This value must contain only letters, numbers, dots, underscores, dashes, slashes or tildes.'),
+        )
+    except TypeError:
+        # Django >= 1.6 'error_messages' dict rather than 'error_message' kw
+        url = forms.RegexField(
+            label=_('URL'),
+            max_length=255,
+            regex=r'^[-\w/\.~]+$',
+            help_text = _(
+                u'Example: /templates/ap/hosted2/ (leading/trailing slashes required)'
+            ),
+            error_messages = {
+                'invalid': _(u'This value must contain only letters, numbers, dots, underscores, dashes, slashes or tildes.'),
+            },
+        )
+
     
     class Meta:
         model = models.Page
+        # Added fields to keep Django >= 1.6 happy
+        fields = (
+            'name',
+            'url',
+            'template_name',
+            'status',
+            'is_active',
+            'publish_date',
+            'expiration_date',
+            'login_required',
+            'author',
+            'notes',
+            'head_title',
+            'head_title_extra',
+            'head_description',
+            'head_keywords',
+            'head_extra',
+            'title',
+            'content',
+            'description_short',
+            'description_long',
+            'extra',
+        )
