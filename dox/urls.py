3556ca887364dac48065c7096f667565748cadde
John Heasly <jheasly@earthlink.net>
2021-06-09 15:02:10 -0700

    Redirect carrier page to https://www.registerguard.com

--- dox/urls.py
+++ dox/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('dox.views',
     
+    (r'^carrier/$', redirect_to, { 'url': 'https://www.registerguard.com', 'permanent':True },),    
     (r'^(?P<url>.*)$', 'page'),
     
 )
